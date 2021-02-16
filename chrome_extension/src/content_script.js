b3c39c3d91045b2be75be6efb33e3c83a502c493
md-prog <26860200+md-prog@users.noreply.github.com>
2021-02-16 17:53:34 -0500

    - UI/UX improvement - caching mechanism added for calling Open.AI API

--- chrome_extension/src/content_script.js
+++ chrome_extension/src/content_script.js
@@ -9,6 +9,7 @@
   var ghostFace; // root element of component
   var triggerBtn; // trigger button for ghost writer
   var ghostAnswerPanel; // panel that holds answers from ghost writer
+  var ghostBelly = [];
 
   /**
    * show FAB button for the extension
@@ -20,12 +21,13 @@
           <svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 92 92" enable-background="new 0 0 92 92" xml:space="preserve" class="arrow-icon show">
             <path id="XMLID_507_" d="M49.9,88c-0.2,0-0.4,0-0.6-0.1c-1.8-0.3-3.2-1.7-3.3-3.5l-3.5-34.8L7.6,46.1c-1.8-0.2-3.3-1.6-3.5-3.3c-0.3-1.8,0.7-3.5,2.3-4.3l76-34.1c1.5-0.7,3.3-0.4,4.5,0.8c1.2,1.2,1.5,3,0.8,4.5l-34.1,76C52.9,87.1,51.4,88,49.9,88z M23.3,39.7L46.4,42c1.9,0.2,3.4,1.7,3.6,3.6l2.4,23.1L76,16L23.3,39.7z"></path>
           </svg>
+          <div class="dot-loading dot-pulse"></div>
         </div>
-        <div class="panel">
-          <div class="panel-header">
+        <div class="gwf-panel">
+          <div class="gwf-panel-header">
             Ghost writer suggests:
           </div>
-          <div class="panel-body">
+          <div class="gwf-panel-body">
             <ul class="gwf-candidates"></ul>
           </div>
         </div>
@@ -35,35 +37,113 @@
 
       ghostFace = document.querySelector(".gwf");;
       triggerBtn = document.querySelector(".gwf .gwf-btn");
-      ghostAnswerPanel = document.querySelector(".gwf .panel");
+      ghostAnswerPanel = document.querySelector(".gwf .gwf-panel");
 
-      triggerBtn.addEventListener('click', function(event) {
+      triggerBtn.addEventListener('click', (event) => {
         event.stopPropagation();
         debouncedCreateCompletion(getSelectedText());
       });
-      ghostAnswerPanel.addEventListener('click', function(event) {
+      document.addEventListener('click', () => toggleAnswerPanel(!1));
+      ghostFace.addEventListener('click', (event) => {
         event.stopPropagation();
-      });
-      document.addEventListener('click', function() {
-        toggleAnswerPanel(!1);
+        var target = event.target;
+        while(target) {
+          if (target.matches('.gwf-answer')) {
+            if (event.pageX == null && event.clientX != null) {
+              eventDoc = (event.target && event.target.ownerDocument) || document;
+              doc = eventDoc.documentElement;
+              body = eventDoc.body;
+  
+              event.pageX = event.clientX +
+                (doc && doc.scrollLeft || body && body.scrollLeft || 0) -
+                (doc && doc.clientLeft || body && body.clientLeft || 0);
+              event.pageY = event.clientY +
+                (doc && doc.scrollTop  || body && body.scrollTop  || 0) -
+                (doc && doc.clientTop  || body && body.clientTop  || 0 );
+            }
+            answerClicked(target, event);
+          }
+          target = target.parentElement;
+        }
       });
     }
 
     var selRect = getSelectedTextPosition();
     if (selRect) {
-      ghostFace.style.left = window.scrollX + selRect.right+'px';
-      ghostFace.style.top = window.scrollY + selRect.bottom+'px';
+      ghostFace.style.left = (window.scrollX + selRect.right) + 'px';
+      ghostFace.style.top = (window.scrollY + selRect.bottom) + 'px';
     }
   }
 
+  function answerClicked(answerEl, evt) {
+    copyTextToClipboard(answerEl.innerText);
+    whisper('Copied into the clipboard!', {x: evt.pageX, y: evt.pageY});
+  }
+
+  function copyTextToClipboard(text) {
+		var textArea = document.createElement("textarea");
+
+		textArea.style.position = 'fixed';
+		textArea.style.top = 0;
+		textArea.style.left = 0;
+    textArea.style.padding = 0;
+    textArea.style.border = 'none';
+		textArea.style.outline = 'none';
+		textArea.style.boxShadow = 'none';
+
+		// Ensure it has a small width and height. Setting to 1px / 1em
+		// doesn't work as this gives a negative w/h on some browsers.
+		textArea.style.width = '2em';
+		textArea.style.height = '2em';
+
+		// Avoid flash of white box if rendered for any reason.
+		textArea.style.background = 'transparent';
+
+		textArea.value = text;
+
+		document.body.appendChild(textArea);
+
+		textArea.select();
+
+    var copied = false;
+		try {
+			document.execCommand('copy');
+      success = true;
+		} catch (err) {
+		} finally {
+			document.body.removeChild(textArea);
+		}
+    return copied;
+	}
+
   function toggleAnswerPanel(show) {
-    if (show) {
-      triggerBtn.classList.add("active");
-      ghostAnswerPanel.classList.add("active");
-    } else {
-      triggerBtn.classList.remove("active");
-      ghostAnswerPanel.classList.remove("active");
+    if (ghostFace) {
+      if (show) {
+        ghostFace.classList.add("gwf-view-panel");
+      } else {
+        ghostFace.classList.remove("gwf-view-panel");
+      }
     }
+  }
+
+  function whisper(text, pos) {
+    var el = document.createElement('span');
+    el.innerText = text;
+    el.style.position = 'fixed';
+    el.style.opacity = 1;
+    el.style.zIndex = 1000;
+    ghostFace.appendChild(el);
+    var rect = el.getBoundingClientRect();
+    el.style.left = (pos.x - rect.width / 2) + 'px';
+    el.style.top = (pos.y - rect.height - window.scrollY) + 'px';
+
+    // animate
+    el.classList.add('gwf-whisper');
+    setTimeout(() => el.remove(), 500);
+    setTimeout(() => {
+      el.style.top = (pos.y - 3 * rect.height - window.scrollY) + 'px';
+      el.style.opacity = 0;
+    }, 0);
   }
 
   function getSelectedText() {
@@ -123,47 +203,68 @@
   }
 
   async function CreateCompletion(text) {
-    console.log('CreateCompletion', text);
-    // call OpenAI API
-    // @see https://beta.openai.com/docs/api-reference/create-completion
-    var response = await fetch('https://api.openai.com/v1/engines/davinci/completions', {
-      method: 'POST', // *GET, POST, PUT, DELETE, etc.
-      mode: 'cors', // no-cors, *cors, same-origin
-      cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
-      credentials: 'same-origin', // include, *same-origin, omit
-      headers: {
-        'Content-Type': 'application/json',
-        'Authorization': 'Bearer ' + API_KEY
-      },
-      redirect: 'follow', // manual, *follow, error
-      referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
-      body: JSON.stringify({
-        "prompt": text,
-        "max_tokens": 100 // default value (One token is roughly 4 characters for normal English text)
-      }) // body data type must match "Content-Type" header
+    if (ghostFace.classList.contains('gwf-thinking'))
+      return;
+
+    ghostFace.classList.add('gwf-thinking');
+    var json = null;
+
+    // check cache
+    var cached = ghostBelly.find((c) => c.promptText == text);
+    cached && (json = cached.response);
+
+    if (!json) {
+      // call OpenAI API
+      // @see https://beta.openai.com/docs/api-reference/create-completion
+      var response = await fetch('https://api.openai.com/v1/engines/davinci/completions', {
+        method: 'POST', // *GET, POST, PUT, DELETE, etc.
+        mode: 'cors', // no-cors, *cors, same-origin
+        cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+        credentials: 'same-origin', // include, *same-origin, omit
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': 'Bearer ' + API_KEY
+        },
+        redirect: 'follow', // manual, *follow, error
+        referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+        body: JSON.stringify({
+          prompt: text,
+          max_tokens: 100, // default value (One token is roughly 4 characters for normal English text)
+          temperature: 0.9,
+          n: 2
+        }) // body data type must match "Content-Type" header
+      });
+      var json = await response.json();
+    }
+
+    ghostBelly.push({
+      promptText: text,
+      response: json
     });
-    var json = await response.json();
 
     if (json.choices) {
       var choicesHtml = '';
-      json.choices.forEach(function(c) {
-        choicesHtml += `<li><p>${c.text}</p></li>`;
+      json.choices.forEach((c) => {
+        choicesHtml += `<li class="gwf-answer"><p>${c.text}</p></li>`;
       });
-      ghostAnswerPanel.querySelector('.panel-body ul').innerHTML = choicesHtml;
+      ghostAnswerPanel.querySelector('.gwf-panel-header').innerHTML = 'Ghost writer has ' + json.choices.length + (json.choices.length == 1 ? ' suggestion' : ' suggestions') + ':';
+      ghostAnswerPanel.querySelector('ul.gwf-candidates').innerHTML = choicesHtml;
     } else {
-      ghostAnswerPanel.querySelector('.panel-body ul').innerHTML = '<li disabled>No candidates!</li>'
+      ghostAnswerPanel.querySelector('ul.gwf-candidates').innerHTML = '<li class="gwf-answer" disabled>No candidates!</li>'
     }
 
     // show "answer panel"
+    ghostFace.classList.remove('gwf-thinking');
     toggleAnswerPanel(!0);
   }
 
   /**
    * Bring in Ghost writer to auto-complete from the current lead.
+   * At least 30 characters must be selected.
    */
   function bringGhostWriter() {
     var selectedText = getSelectedText();
-    if (selectedText) {
+    if (selectedText && selectedText.length > 30) {
       debouncedInitFabButton();
     }
   }
@@ -181,14 +282,12 @@
       var target = getSelectedElement();
       target = target.focusNode || target.anchorNode || target;
       while (target) {
-        console.log(typeof target, target);
         if (target === ghostFace)
           return isPartOfGhostFace = !0;
         target = target.parentElement;
       }
     }
 
-    console.log('isPartOfGhostFace', isPartOfGhostFace);
     if (!isPartOfGhostFace) {
       var selectedText = getSelectedText();
       if (selectedText) {
@@ -201,9 +300,7 @@
 
   function initContentScript() {
     // addEventListener version
-    document.addEventListener('selectionchange', (evt) => {
-      debouncedOnSelectionChange(evt);
-    });
+    document.addEventListener('selectionchange', (evt) => debouncedOnSelectionChange(evt));
   }
 
   const debouncedCreateCompletion = debounce(CreateCompletion, 500);
@@ -214,16 +311,15 @@
   initContentScript();
 
   chrome.extension.sendMessage({}, function(response) {
-    var readyStateCheckInterval = setInterval(function() {
-    if (document.readyState === "complete") {
-      clearInterval(readyStateCheckInterval);
-  
-      // ----------------------------------------------------------
-      // This part of the script triggers when page is done loading
-      console.log("Hello. This message was sent from scripts/content_script.js");
-      // ----------------------------------------------------------
-  
-    }
+    var readyStateCheckInterval = setInterval(() => {
+      if (document.readyState === "complete") {
+        clearInterval(readyStateCheckInterval);
+    
+        // ----------------------------------------------------------
+        // This part of the script triggers when page is done loading
+        console.log("Hello. This message was sent from scripts/content_script.js");
+        // ----------------------------------------------------------
+      }
     }, 10);
   });
 
