3e2664c97eace3a88b7f5057253477c9030c5ac4
klokt-valg <20753260+klokt-valg@users.noreply.github.com>
2021-05-10 05:37:32 -0400

    - vscode editor settings - react native app: api client minor update

--- react_native/components/lib/api-client.ts
+++ react_native/components/lib/api-client.ts
@@ -13,7 +13,7 @@ import {
 
 class MyApiClient {
 
-  MY_API_BASE_URL = "";
+  MY_API_BASE_URL = "http://localhost:3000";
   API_KEY = "";
   DEFAULT_ENGINE : EngineID = EngineID.Curie;
   FILTER_EMPTY_COMPLETION_ANSWER = true;
@@ -37,8 +37,8 @@ class MyApiClient {
    * List engines
    * @see https://beta.openai.com/docs/api-reference/engines
    */
-  public async listEngines() : Promise<ListEnginesResponse> {
-    let json = await this._doGet<ListEnginesResponse>(this.MY_API_BASE_URL + '/openai/engines');  
+  public async listOpenAiEngines() : Promise<ListEnginesResponse> {
+    let json = await this._doGet<ListEnginesResponse>('/openai/engines');  
     return json;
   }
 
@@ -47,7 +47,7 @@ class MyApiClient {
    * @see https://beta.openai.com/docs/api-reference/create-completion
    */
   public async completion(params: CompletionParams) : Promise<CompletionResponse> {
-    let completionUrl = this.MY_API_BASE_URL + '/openai/engines/'+ this.DEFAULT_ENGINE +'/completions';
+    let completionUrl = '/openai/engines/'+ this.DEFAULT_ENGINE +'/completions';
     let json = await this._doPost<CompletionParams, CompletionResponse>(completionUrl, params);  
 
     if (this.FILTER_EMPTY_COMPLETION_ANSWER)
@@ -69,7 +69,7 @@ class MyApiClient {
       text: text
     } as TwinwordTopicTaggingGenerateRequest;
 
-    return await this._doPost<TwinwordTopicTaggingGenerateRequest, TwinwordTopicTaggingGenerateResponse>(this.MY_API_BASE_URL + '/rapidapi/twinword-topic-tagging/generate/', params);
+    return await this._doPost<TwinwordTopicTaggingGenerateRequest, TwinwordTopicTaggingGenerateResponse>('/rapidapi/twinword-topic-tagging/generate/', params);
   }
 
   //
@@ -81,7 +81,7 @@ class MyApiClient {
       sentnum: sentnum ? sentnum : 5,
     } as TextAnalysisTextSummarizationTextRequest;
 
-    return await this._doPost<TextAnalysisTextSummarizationTextRequest, TextAnalysisTextSummarizationResponse>(this.MY_API_BASE_URL + '/rapidapi/textanalysis-text-summarization/text-summarizer-text', params);
+    return await this._doPost<TextAnalysisTextSummarizationTextRequest, TextAnalysisTextSummarizationResponse>('/rapidapi/textanalysis-text-summarization/text-summarizer-text', params);
   }
 
   //
@@ -95,7 +95,7 @@ class MyApiClient {
       strength: strength ? strength : 3,
     } as SmodinRewriteRequest;
 
-    return await this._doPost<SmodinRewriteRequest, SmodinRewriteResponse>(this.MY_API_BASE_URL + '/rapidapi/rewriter-paraphraser-text-changer-multi-language/rewrite', params);
+    return await this._doPost<SmodinRewriteRequest, SmodinRewriteResponse>('/rapidapi/rewriter-paraphraser-text-changer-multi-language/rewrite', params);
   }
   
   //
@@ -109,14 +109,14 @@ class MyApiClient {
     if (this.DEBUG)
       console.log(url, params);
 
-    let response = await fetch(url, {
+    let response = await fetch(this.MY_API_BASE_URL + url, {
       method: 'POST',
       mode: 'cors', // no-cors, *cors, same-origin
       cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
       credentials: 'same-origin', // include, *same-origin, omit
       headers: {
-      'Content-Type': 'application/json',
-      'Authorization': 'Bearer ' + this.API_KEY
+        'Content-Type': 'application/json',
+        'X-MyApi-Key': this.API_KEY,
       },
       redirect: 'follow', // manual, *follow, error
       referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
@@ -137,11 +137,11 @@ class MyApiClient {
     if (this.DEBUG)
       console.log(url);
 
-    let response = await fetch(url, {
+    let response = await fetch(this.MY_API_BASE_URL + url, {
       method: 'GET',
       headers: {
       'Content-Type': 'application/json',
-      'Authorization': 'Bearer ' + this.API_KEY
+      'X-MyApi-Key': this.API_KEY,
       },
     });
 
