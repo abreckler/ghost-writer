7c620ee99293675e7bf0f7a8c736e1a9ace99227
klokt-valg <20753260+klokt-valg@users.noreply.github.com>
2021-06-28 02:08:36 -0400

    article rewriter paragraph improvement

--- server/src/services/article-rewriter/article-rewriter.controller.ts
+++ server/src/services/article-rewriter/article-rewriter.controller.ts
@@ -1,6 +1,7 @@
 import { Request, Response, NextFunction } from 'express';
 import { PipfeedArticleDataExtractorApiClient, SmodinRewriterApiClient } from '../../lib/rapidapi';
 import { splitText } from '../../lib/utils';
+import { htmlToText, HtmlToTextOptions } from 'html-to-text';
 
 const RAPIDAPI_API_KEY = process.env.RAPIDAPI_API_KEY || '';
 
@@ -20,7 +21,23 @@ const rewriteArticle = async (req: Request, res: Response, next: NextFunction) =
     if (params.url) {
       const extractorClient = new PipfeedArticleDataExtractorApiClient(RAPIDAPI_API_KEY);
       const extractorResponse = await extractorClient.extractArticleData(params.url);
-      text = extractorResponse.text || extractorResponse.summary;
+      if (extractorResponse.html) {
+        // use html-to-text to extract text with paragraphs
+        const htmlToTextOptions = {
+          wordwrap: 80, // null for no-wrap
+          ignoreHref: false,
+          ignoreImage: false,
+          singleNewLineParagraphs: false,
+        } as HtmlToTextOptions;
+        text = htmlToText(extractorResponse.html, htmlToTextOptions);
+      }
+      if (!text) {
+        // if we can't use html-to-text, fallback to the raw text or the summary (NOTE: this text does not have paragraphs)
+        text = extractorResponse.text || extractorResponse.summary;
+      }
+      if (extractorResponse.title) {
+        text = extractorResponse.title + '\n\n\n' + text;
+      }
     } else if (params.text) {
       text = params.text;
     }
